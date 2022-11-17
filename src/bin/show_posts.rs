b652b13cefb4fc736c50f38a3fefc7381520d485
Matthew-Mu <matty.mu96@gmail.com>
2022-11-17 15:50:31 +0900

    changes

--- src/bin/show_posts.rs
+++ src/bin/show_posts.rs
@@ -7,6 +7,7 @@ fn main() {
 
     let connection = &mut establish_connection();
     let results = posts
+    // .filter filters out unpublished posts
         .filter(published.eq(true))
         .limit(5)
         .load::<Post>(connection)
@@ -16,6 +17,6 @@ fn main() {
     for post in results {
         println!("{}", post.title);
         println!("----------\n");
-        println!("{}", post.body);
+        println!("{}\n", post.body);
     }
 }
